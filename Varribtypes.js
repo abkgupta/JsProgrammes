50757b7336622e7b54a9817e8dbe673bb1bb3f3d
abhishek <abk1379gupta@gmail.com>
2023-07-07 17:32:03 +0530

    cookies and spreadop

---
+++ Varribtypes.js
@@ -0,0 +1,16 @@
+// If you never want a variable to change, const is the keyword to 
+// use. If you want to reassign values: and you want the hoisting behavior, var is the keyword
+// to use. if you don't want it, let is the keyword for you.
+var a = 34
+a = "scs"
+console.log(a);
+
+// for let variable
+let b = 34
+b = "fvdsdvrge"
+console.log(b);
+
+// for const variable
+const c = 34
+// c = "scs"
+console.log(c);
